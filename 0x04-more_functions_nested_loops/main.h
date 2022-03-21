2a73081a970f82c942f1d2c488a81f6ec7dbd6f7
root <stevanion2013@gmail.com>
2022-03-21 16:00:29 -0700

    function that prints the numbers, from 0 to 9, followed by a new line commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#defind _MAIN_H_
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,6 +11,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(char);
+int _putchar(char c);
+void print_number(int n);
 
-
+#endif
