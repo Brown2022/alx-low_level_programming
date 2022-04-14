0678d82afd360f41e4676dfdfc1038acd9a063c2
root <stevanion2013@gmail.com>
2022-04-14 14:57:22 -0700

    function that returns the sum of all its parameters commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,6 +10,6 @@ iint sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+int putchar(char)
 
 #endif
